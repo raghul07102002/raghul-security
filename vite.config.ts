b91beee8555fac0727dbc6a855f28409ccb4e9fa
raghul07102002 <132688008+raghul07102002@users.noreply.github.com>
2025-10-26 21:43:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/raghul-security/" : "/", // <-- add this
   server: {
     host: "::",
     port: 8080,
